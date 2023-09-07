8f06f5b726354c8047d8033eca9272b0f37ab492
chrisIyer <hellohotbox420@gmail.com>
2023-09-07 10:43:09 -0400

    QUICK EVAL: the first page, a table. The table seems to serve no purpose. Aside from the example of an editable table I wanted to share with Stewart (he said he wanted to be able to make quick edits), it has no functionality. It simply resets the data, It is not connected to  the database. The revenue tab works in that it collects data for the database and displays it. Stewart would like this database to be populated so that input fields are pre-populated to minimize user errors. This database is the actual store. Photos of products need to be captured. Meaning the model needs an image field. And the page needs image functionality. The expense page works in a similair way, however Stewart wants this customer data to be hardcoded in to prepopulate user input fields. The new Account page does all of this, making the expense page unnecessary. The new patient fields should go into the user model. They can be optional fields, so the main user can do admin stuff.

---
+++ config/store.js
@@ -0,0 +1,381 @@
+const saleItems = [
+     {
+       "product": "Raw Flower",
+       "detail": "R4",
+       "unitMeasure": "oz",
+       "unitsAvailable": "-",
+       "unitCost": "-",
+       cost: "-",
+       "recipe=R": ""
+     },
+     {
+       "product": "Raw Flower",
+       "detail": "ACDC",
+       "unitMeasure": "oz",
+       "unitsAvailable": "-",
+       "unitCost": "-",
+       cost: "-",
+       "recipe=R": ""
+     },
+     {
+       "product": "Raw Flower",
+       "detail": "MW",
+       "unitMeasure": "oz",
+       "unitsAvailable": "-",
+       "unitCost": "-",
+       cost: "-",
+       "recipe=R": ""
+     },
+     {
+       "product": "Raw Flower",
+       "detail": "DD",
+       "unitMeasure": "oz",
+       "unitsAvailable": "-",
+       "unitCost": "-",
+       cost: "-",
+       "recipe=R": ""
+     },
+     {
+       "product": "Raw Flower",
+       "detail": "MW/DHDS",
+       "unitMeasure": "oz",
+       "unitsAvailable": "-",
+       "unitCost": "-",
+       cost: "-",
+       "recipe=R": ""
+     },
+     {
+       "product": "Raw Flower",
+       "detail": "LemD",
+       "unitMeasure": "oz",
+       "unitsAvailable": "-",
+       "unitCost": "-",
+       cost: "-",
+       "recipe=R": ""
+     },
+     {
+       "product": "Raw Flower",
+       "detail": "Sativa",
+       "unitMeasure": "oz",
+       "unitsAvailable": "-",
+       "unitCost": "-",
+       cost: "-",
+       "recipe=R": ""
+     },
+     {
+       "product": "Raw Flower",
+       "detail": "Indica",
+       "unitMeasure": "oz",
+       "unitsAvailable": "-",
+       "unitCost": "-",
+       cost: "-",
+       "recipe=R": ""
+     },
+     {
+       "product": "Raw Flower",
+       "detail": "Cotton candy",
+       "unitMeasure": "oz",
+       "unitsAvailable": "-",
+       "unitCost": "-",
+       cost: "-",
+       "recipe=R": ""
+     },
+     {
+       "product": "Raw Flower",
+       "detail": "Cheese",
+       "unitMeasure": "oz",
+       "unitsAvailable": "-",
+       "unitCost": "-",
+       cost: "-",
+       "recipe=R": ""
+     },
+     {
+       "product": "Raw Flower",
+       "detail": "Lilac diesel",
+       "unitMeasure": "oz",
+       "unitsAvailable": "-",
+       "unitCost": "-",
+       cost: "-",
+       "recipe=R": ""
+     },
+     {
+       "product": "Raw Flower",
+       "detail": "Citral glue",
+       "unitMeasure": "oz",
+       "unitsAvailable": "-",
+       "unitCost": "-",
+       cost: "-",
+       "recipe=R": ""
+     },
+     {
+       "product": "Gummies",
+       "detail": "Sleep CBG",
+       "unitMeasure": "1 oz per pack-packs",
+       "unitsAvailable": "12.00",
+       "unitCost": "-",
+       cost: "-",
+       "recipe=R": "R"
+     },
+     {
+       "product": "Gummies",
+       "detail": "Sleep CBD/ACDC",
+       "unitMeasure": "1 oz per pack-packs",
+       "unitsAvailable": "2.00",
+       "unitCost": "-",
+       cost: "-",
+       "recipe=R": "R"
+     },
+     {
+       "product": "Gummies",
+       "detail": "Sleep Indica",
+       "unitMeasure": "1 oz per pack-packs",
+       "unitsAvailable": "2.00",
+       "unitCost": "-",
+       cost: "-",
+       "recipe=R": "R"
+     },
+     {
+       "product": "Gummies",
+       "detail": "Lift",
+       "unitMeasure": "1 oz per pack-packs",
+       "unitsAvailable": "8.00",
+       "unitCost": "-",
+       cost: "-",
+       "recipe=R": "R"
+     },
+     {
+       "product": "Gummies",
+       "detail": "Recovery",
+       "unitMeasure": "1 oz per pack-packs",
+       "unitsAvailable": "2.00",
+       "unitCost": "-",
+       cost: "-",
+       "recipe=R": "R"
+     },
+     {
+       "product": "Gummies",
+       "detail": "Daytime CBG",
+       "unitMeasure": "1 oz per pack-packs",
+       "unitsAvailable": "14.00",
+       "unitCost": "-",
+       cost: "-",
+       "recipe=R": "R"
+     },
+     {
+       "product": "Candies",
+       "detail": "CBD/CBN",
+       "unitMeasure": "oz",
+       "unitsAvailable": "4.00",
+       "unitCost": "-",
+       cost: "-",
+       "recipe=R": "R"
+     },
+     {
+       "product": "Candies",
+       "detail": "Lemongrass",
+       "unitMeasure": "oz",
+       "unitsAvailable": "34.00",
+       "unitCost": "-",
+       cost: "-",
+       "recipe=R": "R"
+     },
+     {
+       "product": "Candies",
+       "detail": "Watermelon",
+       "unitMeasure": "oz",
+       "unitsAvailable": "26.00",
+       "unitCost": "-",
+       cost: "-",
+       "recipe=R": "R"
+     },
+     {
+       "product": "Candies",
+       "detail": "Woe",
+       "unitMeasure": "oz",
+       "unitsAvailable": "34.00",
+       "unitCost": "-",
+       cost: "-",
+       "recipe=R": "R"
+     },
+     {
+       "product": "Candies",
+       "detail": "CBG",
+       "unitMeasure": "oz",
+       "unitsAvailable": "11.00",
+       "unitCost": "-",
+       cost: "-",
+       "recipe=R": "R"
+     },
+     {
+       "product": "Tinctures-Base Oil",
+       "detail": "Indica glycerin",
+       "unitMeasure": "oz",
+       "unitsAvailable": "",
+       "unitCost": "-",
+       cost: "-",
+       "recipe=R": "R"
+     },
+     {
+       "product": "Tinctures-Base Oil",
+       "detail": "Sativa",
+       "unitMeasure": "oz",
+       "unitsAvailable": "9.00",
+       "unitCost": "-",
+       cost: "-",
+       "recipe=R": "R"
+     },
+     {
+       "product": "Tinctures-Base Oil",
+       "detail": "Union custom",
+       "unitMeasure": "oz",
+       "unitsAvailable": "11.00",
+       "unitCost": "-",
+       cost: "-",
+       "recipe=R": "R"
+     },
+     {
+       "product": "Tinctures-Base Oil",
+       "detail": "CBN MCT",
+       "unitMeasure": "oz",
+       "unitsAvailable": "2.00",
+       "unitCost": "-",
+       cost: "-",
+       "recipe=R": "R"
+     },
+     {
+       "product": "Tinctures-Base Oil",
+       "detail": "Chaga",
+       "unitMeasure": "oz",
+       "unitsAvailable": "",
+       "unitCost": "-",
+       cost: "-",
+       "recipe=R": "R"
+     },
+     {
+       "product": "Topicals",
+       "detail": "Garden of Bloom",
+       "unitMeasure": "oz",
+       "unitsAvailable": "2.00",
+       "unitCost": "-",
+       cost: "-",
+       "recipe=R": "R"
+     },
+     {
+       "product": "Topicals",
+       "detail": "Garden of Peace",
+       "unitMeasure": "oz",
+       "unitsAvailable": "5.00",
+       "unitCost": "-",
+       cost: "-",
+       "recipe=R": "R"
+     },
+     {
+       "product": "Topicals",
+       "detail": "Lip Lover",
+       "unitMeasure": "oz",
+       "unitsAvailable": "50.00",
+       "unitCost": "-",
+       cost: "-",
+       "recipe=R": "R"
+     },
+     {
+       "product": "Topicals",
+       "detail": "Massage Oil Cream",
+       "unitMeasure": "oz",
+       "unitsAvailable": "",
+       "unitCost": "-",
+       cost: "-",
+       "recipe=R": "R"
+     },
+     {
+       "product": "Topicals",
+       "detail": "No 10",
+       "unitMeasure": "oz",
+       "unitsAvailable": "2.00",
+       "unitCost": "-",
+       cost: "-",
+       "recipe=R": "R"
+     },
+     {
+       "product": "Topicals",
+       "detail": "Therapeutic Touch",
+       "unitMeasure": "oz",
+       "unitsAvailable": "2.00",
+       "unitCost": "-",
+       cost: "-",
+       "recipe=R": ""
+     },
+     {
+       "product": "Topicals",
+       "detail": "Soaps",
+       "unitMeasure": "each",
+       "unitsAvailable": "12.00",
+       "unitCost": "-",
+       cost: "-",
+       "recipe=R": "R"
+     },
+     {
+       "product": "Topicals",
+       "detail": "TLC Cream Small",
+       "unitMeasure": "oz",
+       "unitsAvailable": "22.00",
+       "unitCost": "-",
+       cost: "-",
+       "recipe=R": "R"
+     },
+     {
+       "product": "Topicals",
+       "detail": "TLC Cream Large",
+       "unitMeasure": "oz",
+       "unitsAvailable": "",
+       "unitCost": "-",
+       cost: "-",
+       "recipe=R": "R"
+     },
+     {
+       "product": "Topicals",
+       "detail": "Bath Balms sm",
+       "unitMeasure": "oz",
+       "unitsAvailable": "1.00",
+       "unitCost": "-",
+       cost: "-",
+       "recipe=R": "R"
+     },
+     {
+       "product": "Topicals",
+       "detail": "Bath Balms lg",
+       "unitMeasure": "oz",
+       "unitsAvailable": "",
+       "unitCost": "-",
+       cost: "-",
+       "recipe=R": ""
+     },
+     {
+       "product": "Marshmallows",
+       "detail": "Indica",
+       "unitMeasure": "each",
+       "unitsAvailable": "44.00",
+       "unitCost": "-",
+       cost: "-",
+       "recipe=R": "R"
+     },
+     {
+       "product": "Marshmallows",
+       "detail": "Sative",
+       "unitMeasure": "each",
+       "unitsAvailable": "73.00",
+       "unitCost": "-",
+       cost: "-",
+       "recipe=R": "R"
+     },
+     {
+       "product": "Marshmallows",
+       "detail": "CBG",
+       "unitMeasure": "each",
+       "unitsAvailable": "",
+       "unitCost": "-",
+       cost: "-",
+       "recipe=R": "R"
+     }
+   ]
+   

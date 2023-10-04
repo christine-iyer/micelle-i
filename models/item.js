02b747befbd590610d6277817a5ca782458b9d1c
chrisIyer <hellohotbox420@gmail.com>
2023-10-04 16:53:01 -0400

    cleaning up.

--- models/item.js
+++
@@ -1,4 +0,0 @@
-const mongoose = require('mongoose')
-require('./category')
-const itemSchema = require('./itemSchema')
-module.exports = mongoose.model('Item', itemSchema)
--- src/pages/App/App.js
+++ src/pages/App/App.js
@@ -6,7 +6,7 @@ import Revenue from '../../pages/Revenue'
 import Expenses from '../../pages/Expenses'
 import NewPatient from '../../pages/NewPatient';
 import NewItem from '../../pages/NewItem'
-import Table from '../../pages/Table'
+import TableCell from '../../components/Table/TableCell'
 import { Routes, Route, BrowserRouter } from "react-router-dom"
 import Inventory from '../../pages/Inventory'
 import makeData from '../../makeData';
@@ -22,7 +22,7 @@ function App() {
         <BrowserRouter>
           <Bar />
           <Routes>
-            <Route path="/" element={<Table />}/>
+            <Route path="/" element={<TableCell />}/>
             <Route path="/expenses" element={<Expenses />} />
             <Route path="/revenues" element={<Revenue />} />
             <Route path="/inventorys" element={<NewItem />} />

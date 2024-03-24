3eb17ff27724ad1c193c3f97c7a426bbadeefe71
chrisIyer <hellohotbox420@gmail.com>
2024-03-24 17:46:33 -0400

    AG grid is not working yet.

--- src/pages/NewAccount.js
+++ src/pages/NewAccount.js
@@ -1,38 +1,44 @@
 import { useState, useEffect, useRef } from 'react'
+import { AgGridReact } from 'ag-grid-react';
+import 'ag-grid-community/styles/ag-grid.css';
+import 'ag-grid-community/styles/ag-theme-quartz.css';
+
 import './../App.css'
+
+
 
 export const COLUMNS = [
     {
-        Header: 'Name',
-        accessor: 'name',
+        headerName: 'Name',
+        flex: 2, filter: true, editable: true
     },
     {
-        Header: 'Email',
-        accessor: 'email',
+        headerName: 'Email',
+        flex: 2, filter: true, editable: true
     },
     {
-        Header: 'Phone',
-        accessor: 'phone',
+        headerName: 'Phone',
+        flex: 2, filter: true, editable: true
     },
     {
-        Header: 'Address',
-        accessor: 'address',
+        headerName: 'Address',
+        flex: 2, filter: true, editable: true
     },
     {
-        Header: 'City',
-        accessor: 'city',
+        headerName: 'City',
+        flex: 2, filter: true, editable: true
     },
     {
-        Header: 'State',
-        accessor: 'state',
+        headerName: 'State',
+        flex: 2, filter: true, editable: true
     },
     {
-        Header: 'Zip',
-        accessor: 'zip',
+        headerName: 'Zip',
+        flex: 2, filter: true, editable: true
     },
     {
-        Header: 'Description',
-        accessor: 'description',
+        headerName: 'Description',
+        flex: 2, filter: true, editable: true
     }
 ];
 export default function NewAccts() {
@@ -48,9 +54,43 @@ export default function NewAccts() {
         zip: '',
         description: ''
     })
-    const ref = useRef(null)
-    const inputRef = useRef(null)
-    
+    const [colDef, setColDef] = useState([
+        {
+            headerName: 'Name',
+            flex: 2, filter: true, editable: true
+        },
+        {
+            headerName: 'Email',
+            flex: 2, filter: true, editable: true
+        },
+        {
+            headerName: 'Phone',
+            flex: 2, filter: true, editable: true
+        },
+        {
+            headerName: 'Address',
+            flex: 2, filter: true, editable: true
+        },
+        {
+            headerName: 'City',
+            flex: 2, filter: true, editable: true
+        },
+        {
+            headerName: 'State',
+            flex: 2, filter: true, editable: true
+        },
+        {
+            headerName: 'Zip',
+            flex: 2, filter: true, editable: true
+        },
+        {
+            headerName: 'Description',
+            flex: 2, filter: true, editable: true
+        }
+    ])
+    // const ref = useRef(null)
+    // const inputRef = useRef(null)
+
     const handleChange = (evt) => {
         setNewacct({ ...newacct, [evt.target.name]: evt.target.value })
     }
@@ -139,47 +179,61 @@ export default function NewAccts() {
             {'Description '}<input value={newacct.description} onChange={handleChange} name="description"></input><br />
             <button onClick={() => createNewAcct()}>Create A New New Acct</button>
             {
-                foundNewacct ? <div>
-                    <h2>{foundNewacct.name}</h2>
+                foundNewacct ? 
+                <div 
+                className="ag-theme-quartz" applying the grid theme
+               style={{ height: 500 }} // the grid will fill the size of the parent container
+              >
+                            <AgGridReact
+                rowData={newaccts}
+                columnDefs={colDef}
+            />
+
+
+
+
+                    {/* <h2>{foundNewacct.name}</h2>
                     <h2>{foundNewacct.email}</h2>
                     <h2>{foundNewacct.phone}</h2>
                     <h2>{foundNewacct.address}</h2>
                     <h2>{foundNewacct.city}</h2>
                     <h2>{foundNewacct.state}</h2>
                     <h2>{foundNewacct.zip}</h2>
-                    <h2>{foundNewacct.description}</h2>
+                    <h2>{foundNewacct.description}</h2> */}
                 </div> : <>No New Accounts Found </>
             }
             <hr></hr>
             {
-                newaccts && newaccts.length ? (<ul>
-                    {
-                        newaccts.map((newacct) => {
-                            return (
-                                <div key={newacct._id}>
-                                    <table>
-                                        <thead>
-                                            <tr >
-                                                <td>  {newacct.name}  </td>
-                                                <td>  {newacct.email} </td>
-                                                <td>{newacct.phone}   </td>
-                                                <td> {newacct.address}  </td>
-                                                <td> {newacct.city}  </td>
-                                                <td>  {newacct.state} </td>
-                                                <td>  {newacct.zip} </td>
-                                                <td>  {newacct.description} </td>
-                                                <td>  {newacct.zip} </td>
-                                                <td>  {newacct.description} </td>
-                                                <td> <button onClick={() => deleteNewAcct(newacct._id)}>X</button> </td>
-                                                <td>  <button onClick={() => updateNewAcct(newacct._id)}>Edit</button> </td>
-                                            </tr>
-                                        </thead>
-                                    </table>
-                                </div>
-                            )
-                        })
-                    }
-                </ul>) : <h1>No Expenses Yet Add One</h1>
+                newaccts && newaccts.length ?
+                    (<ul>
+                        {
+                            newaccts.map((newacct) => {
+                                return (
+                                    <div key={newacct._id}>
+                                        <table>
+                                            <thead>
+                                                <tr >
+                                                    <td>  {newacct.name}  </td>
+                                                    <td>  {newacct.email} </td>
+                                                    <td>{newacct.phone}   </td>
+                                                    <td> {newacct.address}  </td>
+                                                    <td> {newacct.city}  </td>
+                                                    <td>  {newacct.state} </td>
+                                                    <td>  {newacct.zip} </td>
+                                                    <td>  {newacct.description} </td>
+                                                    <td>  {newacct.zip} </td>
+                                                    <td>  {newacct.description} </td>
+                                                    <td> <button onClick={() => deleteNewAcct(newacct._id)}>X</button> </td>
+                                                    <td>  <button onClick={() => updateNewAcct(newacct._id)}>Edit</button> </td>
+                                                </tr>
+                                            </thead>
+                                        </table>
+                                    </div>
+                                )
+                            })
+                        }
+                    </ul>)
+                    : <h1>No Expenses Yet Add One</h1>
             }
         </>
     )
